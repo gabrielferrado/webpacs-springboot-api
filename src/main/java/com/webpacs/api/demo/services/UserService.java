5cd3df99a9dac37a8dc2b27d4dba86f4fe57a61d
Francinildo Alves de Lima <franalves1923@gmail.com>
2021-02-11 13:52:05 -0300

    Updating UserService.class
    Providing instance to UserService::userRepository preferring @Autowired instead of default constructor

--- src/main/java/com/webpacs/api/demo/services/UserService.java
+++ src/main/java/com/webpacs/api/demo/services/UserService.java
@@ -8,12 +8,9 @@ import java.util.Optional;
 
 @Service
 public class UserService {
-  private final UserRepository userRepository;
-
-
-  public UserService(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
+ 
+  @Autowired
+  private UserRepository userRepository;
 
   public Optional<User> find(Long id) {
     return userRepository.findById(id);
